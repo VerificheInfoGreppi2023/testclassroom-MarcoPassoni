041b6536d2e9840ebd5cd289433dc3748e0d14ef
MarcoPassoni <marco.passoni@issgreppi.it>
2023-10-02 11:16:56 +0200

    add main_2.js

--- CSS/Lezione2/scripts/main.js
+++ CSS/Lezione2/scripts/main.js
@@ -1,2 +1,9 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myHeading=document.querySelector("h1");
+myHeading.textContent="Hello world!";
+let myVariable="Bob";
+let eta=25;
+let variabile=true;
+let vettore=[2, 3, "Pippo", false];
+let eta1="25";
+let confronto=(eta===eta1);
+alert(confronto);
