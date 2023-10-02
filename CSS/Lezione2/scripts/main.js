aa104aa89f25f9ad1d5c3182abc3f3d1427bb3bd
MarcoPassoni <marco.passoni@issgreppi.it>
2023-10-02 11:53:12 +0200

    add main_2.js

--- CSS/Lezione2/scripts/main.js
+++ CSS/Lezione2/scripts/main.js
@@ -1,9 +1,30 @@
-const myHeading=document.querySelector("h1");
-myHeading.textContent="Hello world!";
-let myVariable="Bob";
-let eta=25;
-let variabile=true;
-let vettore=[2, 3, "Pippo", false];
-let eta1="25";
-let confronto=(eta===eta1);
-alert(confronto);
+// myHeading.textContent="Hello world!";
+// let myVariable="Bob";
+// let eta=25;
+// let variabile=true;
+// let vettore=[2, 3, "Pippo", false];
+// let eta1="25";
+// let confronto=(eta===eta1);
+// alert(confronto);
+
+// function mult(num1, num2){
+//     let result = num1 * num2;
+//     return result;
+// }
+// let ris = mult(3,4);
+
+// document.querySelector("img").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+// });
+
+let myButton = document.querySelector("button")
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
+myButton.onclick=()=>{
+    setUserName()
+}
